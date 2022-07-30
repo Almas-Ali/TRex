48429f583d8581c2d769a84988edd6fd87f6ec3c
Md. Almas Ali <almaspr3@gmail.com>
2022-07-31 02:34:37 +0600

    Meta tags improvements.

---
+++ resources/views/frontend/layouts/seo.blade.php
@@ -0,0 +1,33 @@
+{{-- Open Graph Tags --}}
+
+<meta property="og:type" content="article" />
+<meta property="og:title" content="Sammy the Shark" />
+<meta property="og:description" content="Senior Selachimorpha at DigitalOcean" />
+<meta property="og:url" content="https://html.sammy-codes.com/" />
+<meta property="og:image" content="https://html.sammy-codes.com/images/large-profile.jpg" />
+
+
+{{-- Twitter Card Tags --}}
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:site" content="@digitalocean" />
+<meta name="twitter:title" content="Sammy the Shark" />
+<meta name="twitter:description" content="Senior Selachimorpha at DigitalOcean" />
+<meta name="twitter:image" content="https://html.sammy-codes.com/images/large-profile.jpg" />
+
+
+{{-- Google Tags --}}
+<meta name="description" content="A description of the page" />
+<meta name="google-site-verification" content="+nxGUDJ4QpAZ5l9Bsjdi102tLVC21AIh5d1Nl23908vVuFHs34=" />
+<meta name="robots" content="noindex,nofollow" />
+<meta name="robots" content="..., ..." />
+<meta name="googlebot" content="..., ..." />
+<meta name="google" content="nositelinkssearchbox" />
+<meta name="googlebot" content="notranslate" />
+<meta name="google" content="nopagereadaloud" />
+<meta name="google-site-verification" content="..." />
+<meta http-equiv="Content-Type" content="...; charset=..." />
+<meta charset="..." />
+<meta http-equiv="refresh" content="...;url=..." />
+<meta name="viewport" content="..." />
+<meta name="rating" content="adult" />
+<meta name="rating" content="RTA-5042-1996-1400-1577-RTA" />
